48a24479da00d24044060599abe187ee204d7ca0
Ali <39136692+arkh91@users.noreply.github.com>
2025-05-25 00:47:00 -0700

    fixed

--- Key-Creation.js
+++ Key-Creation.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; // only for testing
 const apiUrl = ''; // Replace with your actual API URL Format: https://1.23.456.789:12345/
-const apiKey = 'OUUogg5tZIsght_nf2lDpQ/'; // Replace with your actual API key Format: https://domain:port/"OUUogg5tZIsght_nf2lDpQ/"
+const apiKey = ''; // Replace with your actual API key Format: https://domain:port/"OUUogg5tZIsght_nf2lDpQ/"
 
 async function createNewKey() {
     try {
         const response = await axios.post(
-            `${apiUrl}/OUUogg5tZIsght_nf2lDpQ/access-keys`,
+            `${apiUrl}/${apiKey}/access-keys`,
             {},
             {
                 headers: {
