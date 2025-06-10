80bc0c7b7dcf7c544fd86b558d0fdb9d7dc0a53e
Ali <39136692+arkh91@users.noreply.github.com>
2025-06-09 22:47:17 -0700

    Create servers.js

---
+++ servers.js
@@ -0,0 +1,10 @@
+// Configuration - adjust these values for your servers
+const SERVERS = {
+    US05: {
+            apiUrl: '', // Full API endpoint https://test.com:11111/
+        apiKey: '', //'NETvlfDZjtlGdAA/',
+        aliases: [''] // All possible domain variations
+    }
+};
+
+module.exports = SERVERS;
