77fb5b1e7b59e44ed996a6aab61f065151278a23
Ali <39136692+arkh91@users.noreply.github.com>
2025-09-07 22:05:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const token = ''; //RoyalVPN
 //const token = ''; //Test
 //const { TELEGRAM_BOT_TOKEN } = require('./token');
 const { NOWPAYMENTS_API_KEY } = require('./token');
-//const NOWPAYMENTS_API_KEY = '4PPCTPB-385MXPM-N5DBCGX-KV64DPY';
+//const NOWPAYMENTS_API_KEY = '';
 
 const createNowPaymentsSession = require('./createNowPaymentsSession');
 
